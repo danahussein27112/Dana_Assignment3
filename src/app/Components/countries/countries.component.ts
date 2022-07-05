4f39e33ba2f57c99b32bc100bdb70513166f9705
danah <danah@DESKTOP-V5470IE>
2022-07-05 11:14:55 +0300

    .

--- src/app/Components/countries/countries.component.ts
+++ src/app/Components/countries/countries.component.ts
@@ -1,49 +1,53 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { Router } from '@angular/router';
-import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs/internal/Observable';
-import { CountryActions } from 'src/app/Store/Actions/Country.actions';
-import { of } from 'rxjs';
-import { Country } from 'src/app/Modules/country/country.model';
+// import { Component, OnInit, ViewChild } from '@angular/core';
+// import { Router } from '@angular/router';
+// import { Store } from '@ngrx/store';
+// import { Observable } from 'rxjs/internal/Observable';
+// import { CountryActions } from 'src/app/Store/Actions/Country.actions';
+// import { of } from 'rxjs';
+// import { Country } from 'src/app/Modules/country/country.model';
 
-@Component({
-  selector: 'countries',
-  templateUrl: './countries.component.html',
-  styleUrls: ['./countries.component.css']
-})
-export class CountriesComponent implements OnInit {
-  countries$?: Observable<Country[]>;
-  error$?: Observable<any>;
-  isLoading$?: Observable<boolean> ;
+// @Component({
+//   selector: 'countries',
+//   templateUrl: './countries.component.html',
+//   styleUrls: ['./countries.component.css']
+// })
+// export class CountriesComponent implements OnInit {
+//   countries$?: Observable<Country[]>;
+//   error$?: Observable<any>;
+//   isLoading$?: Observable<boolean> ;
  
-  constructor(
-      private store$: Store<CountryState>,
-      private router: Router) 
-  {
-  }
+//   constructor(
+//       private store$: Store<CountryState>,
+//       private router: Router) 
+//   {
+//   }
 
-  ngOnInit() {
-      this.store$.dispatch(CountryActions.loadRequestAction());
+//   ngOnInit() {
+//       this.store$.dispatch(CountryActions.loadRequestAction());
 
-      this.store$.select(getCountries).subscribe(res => 
-          {
-               //this .countries$ =res as Observable<Country[]>;
-              this.countries$=of(res)    }
-      );
-     // this.isLoading$ = this.store$.select(getCountryIsLoading) as unknownas;
-  }
+//       this.store$.select(getCountries).subscribe(res => 
+//           {
+//                //this .countries$ =res as Observable<Country[]>;
+//               this.countries$=of(res)    }
+//       );
+//      // this.isLoading$ = this.store$.select(getCountryIsLoading) as unknownas;
+//   }
 
-  onRefresh()
-  {
-      this.store$.dispatch(CountryActions.loadRequestAction());
-  }
+//   onRefresh()
+//   {
+//       this.store$.dispatch(CountryActions.loadRequestAction());
+//   }
 
-  selectById(id:number) {
-      this.router.navigate(['country', id]);     
-  }
+//   selectById(id:number) {
+//       this.router.navigate(['country', id]);     
+//   }
 
-  eventNewRecord(event:any)
-  {
-      this.router.navigate(['create']);     
-  }
-}
+//   eventNewRecord(event:any)
+//   {
+//       this.router.navigate(['create']);     
+//   }
+// }
+
+// function getCountries(getCountries: any) {
+//     throw new Error('Function not implemented.');
+// }
